50e608c9a546fd79e2b5b991a0da7d7a0f81bd98
Udit kathuria <kathuriaudit7@gmail.com>
2026-05-11 00:56:25 +0530

    Initial push of backend/models/RefreshToken.js

---
+++ backend/models/RefreshToken.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const refreshTokenSchema = new mongoose.Schema({
+  token: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  expiryDate: {
+    type: Date,
+    required: true
+  }
+});
+
+refreshTokenSchema.statics.verifyExpiration = (token) => {
+  return token.expiryDate.getTime() < new Date().getTime();
+};
+
+module.exports = mongoose.model('RefreshToken', refreshTokenSchema);
